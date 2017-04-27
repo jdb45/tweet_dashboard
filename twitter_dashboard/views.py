86fbd6f4ba21a7d1b1252568de7c9e9ab6a12c28
Jeremy <acnajcadcac.yahoo.com>
2017-04-26 22:26:39 -0500

    added some comments

--- twitter_dashboard/views.py
+++ twitter_dashboard/views.py
@@ -51,7 +51,7 @@ def tweet_list(request):
     return render(request, 'twitter_dashboard/tweet.html', {'names' : names})
 
 def save_information(request):
-    print(len(info_lists.text_list))
+    # getting the length of the lists
     length = len(info_lists.text_list)
     if len(info_lists.user_name) == 0:
         pass
@@ -62,9 +62,11 @@ def save_information(request):
                 form = TweetForm(request.POST)
                 if form.is_valid():
                     tweet_save = form.save(commit=False)
+                    # adding each element to the database
                     tweet_save.user_name = info_lists.user_name[count]
                     tweet_save.date_tweeted = info_lists.date_tweeted[count]
                     tweet_save.tweet = info_lists.text_list[count]
+                    # making sure the location list does not go past the end point
                     if count < len(info_lists.tweet_locations):
                         tweet_save.location = info_lists.tweet_locations[count]
                     tweet_save.save()
@@ -75,16 +77,18 @@ def save_information(request):
     return redirect('tweet_list')
 
 def open_map_locations(request):
-
+    # getting the map
     geolocator = Nominatim(scheme='http')
     world_map = folium.Map(location=[40, -120], zoom_start=3, tiles='Stamen Terrain')
 
     for i in info_lists.tweet_locations:
+        # making sure there is a location
         if i != '':
             info_lists.location = geolocator.geocode(i)
             if info_lists.location == None:
                 pass
             else:
+                # adding each location to the map
                 marker = folium.Marker([info_lists.location.latitude, info_lists.location.longitude], popup=i)
                 marker.add_to(world_map)
 
@@ -96,6 +100,7 @@ def live_tweets(request):
     # opening and closing the file to clear it for a second search
     open('twitter_dashboard/static/text/system.txt', 'w').close()
     system_output = open('twitter_dashboard/static/text/system.txt', 'r+')
+    # setting all the lists back to empty
     info_lists.location = []
     info_lists.text_list = []
     info_lists.tweet_locations = []
@@ -107,9 +112,7 @@ def live_tweets(request):
             user_search = str(post_request['search']) # getting the user search
             count = 0
             tso.set_keywords([user_search])  # setting the keyword to search for
-            '''stream_listener = StreamListener()
-            stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-            stream.filter(track=[user_search])''' # TODO figure out how to process tweets fast enough to use tweepy
+            # keeping this at a 50 tweet max to not get flagged
             top_fifty_tweets = 50
             for tweet in ts.search_tweets_iterable(tso):
                 count += 1
